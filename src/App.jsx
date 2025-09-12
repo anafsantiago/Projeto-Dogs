60d8b4059f1600423de11035acd60ebffc1e3edd
anafsantiago <fla.santiago09@gmail.com>
2025-09-12 00:16:43 -0300

    estrutura inicial e headers

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import "./App.css";
 import Api from "./api/Api";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
 
 const App = () => {
   return (
-    <div>
-      <Api />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />}/>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 };
 
--- src/api/endpoints/PhotoGet.jsx
+++ src/api/endpoints/PhotoGet.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const PhotoGet = () => {
   const [id, setId] = React.useState("");
+  const [foto, setFoto] = React.useState("");
   function handleSubmit(event) {
     event.preventDefault();
     fetch(`https://dogsapi.origamid.dev/json/api/photo/${id}`)
@@ -11,18 +12,28 @@ const PhotoGet = () => {
       })
       .then((json) => {
         console.log(json);
+        setFoto(json.photo.src);
         return json;
       });
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={id}
-        onChange={({ target }) => setId(target.value)}
-      />
-      <button>Enviar</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={id}
+          onChange={({ target }) => setId(target.value)}
+        />
+        <button>Enviar</button>
+      </form>
+      <div>
+        <img
+          style={{ display: "block", maxWidth: "600px" }}
+          src={foto}
+          alt="Cachorro da raça poodle de cor preta"
+        />
+      </div>
+    </>
   );
 };
 
