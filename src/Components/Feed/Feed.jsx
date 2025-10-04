22d4f97ee9c7f0e4d321b4a210cca25aa5019c10
anafsantiago <fla.santiago09@gmail.com>
2025-10-04 16:27:29 -0300

    criação da animação de Loading

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import FeedModal from "./FeedModal";
 import FeedPhotos from "./FeedPhotos";
+import Loading from "../Helper/Loading";
 
 const Feed = () => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
